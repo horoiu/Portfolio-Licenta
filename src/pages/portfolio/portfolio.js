18d8d5a3755ec3b2c78549e3591f827ae2b484a5
horoiu <marian.horoiu@gmail.comgit>
2020-06-21 00:27:37 +0300

    Working on fetchItems inside portfolio.js. Connected to DB from XAMPP and getting projects on Porftolio page

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -1,11 +1,30 @@
-import React from "react";
-// import "./portfolio.css";
+import React, { useState, useEffect } from "react";
+import "./portfolio.css";
 
 function Portfolio() {
+    // useEfect() is used to run the fechItems function once the Portfolio component is mounted
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    // initialising state with an empty array
+    const [items, setItems] = useState([]);
+
+    const fetchItems = async () => {
+        // const data = await fetch("localhost:4000/proiecte");
+        const data = await fetch("http://localhost:4000/proiecte");
+
+        const items = await data.json();
+        console.log(items.data);
+        setItems(items.data);
+    };
+
     return (
-        <div className="portfolio">
-            <h1>Portfolio Page</h1>
-        </div>
+        <main className="portfolio">
+            {items.map((item, index) => (
+                <h3 key={index}>{item.nume_proiect}</h3>
+            ))}
+        </main>
     );
 }
 
