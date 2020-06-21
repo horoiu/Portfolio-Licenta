40a553d885c754db7ea72dbadabc5472ccb787ca
horoiu <marian.horoiu@gmail.comgit>
2020-06-21 17:55:23 +0300

    Implemented convertDate function in projectDetails.js to convert Date format comming from DB.

--- src/components/projectDetails/projectDetails.js
+++ src/components/projectDetails/projectDetails.js
@@ -15,10 +15,21 @@ class ProjectDetails extends Component {
         } = this.props.data;
         // console.log("projectDetails: this.props=", this.props.data);
 
+        const convertDate = (date) => {
+            let year = new Date(date).getFullYear();
+            let month = new Date(date).getMonth();
+            let day = new Date(date).getDay();
+            let data = `${day}-${month}-${year}`;
+
+            return data;
+        };
+
+        const data = convertDate(data_proiect);
+
         return (
             <div>
                 <h1>{nume_proiect}</h1>
-                <h3>{data_proiect}</h3>
+                <h3>Date: {data}</h3>
                 <h3>ID Proiect = {id_proiect}</h3>
                 <h3>ID Categorie = {id_categ}</h3>
                 <h3>{fisier_imagine}</h3>
