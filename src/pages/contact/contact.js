d09f4f4c5867c7daef5b575747ba2ac505799195
horoiu <marian.horoiu@gmail.comgit>
2020-06-25 00:01:57 +0300

    Worked on Contact page: made responsive, stylings and added Media Icons. Implemented mediaIcons.js list as source for rendered icons.

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,20 +1,43 @@
 import React, { Component } from "react";
 import "./contact.css";
 
+import mediaList from "../../json/mediaIcons.json";
+import { Row } from "react-bootstrap";
+import { MDBIcon } from "mdbreact";
+
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { list: mediaList };
+    }
     render() {
+        let mediaList = this.state.list.map(function (item) {
+            return (
+                <li key={item.name} className="">
+                    <a href={item.url} target="_blank">
+                        <MDBIcon
+                            fab
+                            icon={item.icon}
+                            size="4x"
+                            className="hoverable"
+                        />
+                    </a>
+                </li>
+            );
+        });
+
         return (
-            <main className="contact">
+            <main className="contact mt-5">
                 <div className="bg-info contact4 overflow-hiddedn position-relative">
                     <div className="row no-gutters">
                         <div className="container">
                             <div className="col-lg-6 contact-box mb-4 mb-md-0">
                                 <div className="">
-                                    <h1 className="title font-weight-light text-white mt-2">
+                                    <h1 className="title h1-responsive font-weight-light text-white mt-2">
                                         Contact Us
                                     </h1>
-                                    <form className="mt-3">
-                                        <div className="row">
+                                    <form className="mt-5">
+                                        <Row>
                                             <div className="col-lg-12">
                                                 <div className="form-group mt-2">
                                                     <input
@@ -54,33 +77,38 @@ class Contact extends Component {
                                                     +40 740 123456
                                                 </span>
                                             </div>
-                                        </div>
+                                        </Row>
                                     </form>
                                 </div>
+                                <Row className="">
+                                    <ul className="mediaIcons d-flex justify-content-around mt-5 pr-4">
+                                        {mediaList}
+                                    </ul>
+                                </Row>
                             </div>
                         </div>
-                        <div className="col-lg-6 right-image p-r-0">
-                            <div className="mapouter">
-                                <div className="gmap_canvas mt-4">
-                                    <iframe
-                                        title="geolocation"
-                                        className="mt-5 col-lg-12 col-md-8"
-                                        width="700"
-                                        height="430"
-                                        id="gmap_canvas"
-                                        src="https://maps.google.com/maps?q=Marasti%20Cluj-napoca&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                                        frameborder="0"
-                                        scrolling="no"
-                                        marginheight="0"
-                                        marginwidth="0"
-                                    ></iframe>
+                        <div className="container">
+                            <div className="col-lg-6 right-image p-r-0">
+                                <div className="mapouter">
+                                    <div className="gmap_canvas mt-5">
+                                        <iframe
+                                            title="geolocation"
+                                            className="iframe-map mt-5 mb-5"
+                                            width="700"
+                                            height="430"
+                                            id="gmap_canvas"
+                                            src="https://maps.google.com/maps?q=Marasti%20Cluj-napoca&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                                            frameBorder="0"
+                                            scrolling="no"
+                                            marginHeight="0"
+                                            marginWidth="0"
+                                        ></iframe>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="row no-gutters">Footer</div>
                 </div>
-                {/* </div> */}
             </main>
         );
     }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,8 +4,10 @@ import "./home.css";
 function Home() {
     return (
         <header>
-            <h1>Welcome to DevSpace</h1>
-            <p>This is a Portfolio project using ReactJS</p>
+            <h1 className="h1-responsive mb-3">Welcome to DevSpace</h1>
+            <h3 className="h3-responsive">
+                This is a Portfolio project using ReactJS
+            </h3>
         </header>
     );
 }
