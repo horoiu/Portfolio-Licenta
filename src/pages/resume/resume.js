b83e28884e7e1bd42fb0e3cf00c53acacb85610a
horoiu <marian.horoiu@gmail.comgit>
2020-06-29 01:30:52 +0300

    Working on Resume page, to render a pdf file. In progress...

--- src/pages/resume/resume.js
+++ src/pages/resume/resume.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./resume.css";
 
-function Resume() {
-    return (
-        <div className="resume">
-            <h1>Resume Page</h1>
-        </div>
-    );
+import { Document, Page } from "react-pdf/dist/entry.webpack";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+
+class Resume extends Component {
+    state = { numPages: null, pageNumber: 1 };
+
+    onDocumentLoadSuccess = ({ numPages }) => {
+        this.setState({ numPages });
+    };
+
+    goToPrevPage = () =>
+        this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
+    goToNextPage = () =>
+        this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
+
+    render() {
+        const { pageNumber, numPages } = this.state;
+
+        return (
+            <main className="resume">
+                <nav>
+                    <button onClick={this.goToPrevPage}>Prev</button>
+                    <button onClick={this.goToNextPage}>Next</button>
+                </nav>
+
+                <div style={{ width: 600 }}>
+                    <Document
+                        file="../../../public/CV-2019.pdf"
+                        onLoadSuccess={this.onDocumentLoadSuccess}
+                    >
+                        <Page pageNumber={pageNumber} width={600} />
+                    </Document>
+                </div>
+
+                <p>
+                    Page {pageNumber} of {numPages}
+                </p>
+            </main>
+        );
+    }
 }
 
 export default Resume;
